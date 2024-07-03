a2a3d0c6ebf2644f863bdeaf2e3de2f6324677ba
maGNICHI <manar.gnichi@esprit.tn>
2024-07-03 13:03:27 +0100

    create clear separation between commands and queries using the mediator

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,15 @@
-﻿namespace CatalogAPI.Products.CreateProduct
+﻿using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
 {
-    public class CreateProductHandler
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
