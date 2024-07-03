c0337990888a7ebcc50c4a384b09897c0b1453e3
maGNICHI <manar.gnichi@esprit.tn>
2024-07-03 14:28:12 +0100

    create command and queries interface

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS
+{
+    //doesn't return any response
+    public interface ICommand : ICommand<Unit>
+    {
+    }
+    //that means command will returning this response type
+    //we can pass i request generics
+    //this first abstraction
+    //2st abstraction  define regular interface
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
