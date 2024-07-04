5f29b4b78018acf23787954f46d1bcb492e2e595
maGNICHI <manar.gnichi@esprit.tn>
2024-07-04 13:20:49 +0100

    terminate creation product handler

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,33 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+using MediatR;
 
 namespace CatalogAPI.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-    : IRequest<CreateProductResult>;
+    : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
     internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            //create Product entity from command object
+            //save to database
+            //return CreateProductResult result               
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //save to database
+
+            //return result
+            return new CreateProductResult(product.Id);
         }
     }
 }
